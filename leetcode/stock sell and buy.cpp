9ae57564a65d4e52199c0a9217e4fbb05e23bdbf
Ayush8059 <ayushr8059@gmail.com>
2025-12-20 22:53:58 +0530

    stock buy and sell

---
+++ leetcode/stock sell and buy.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int minPrice = INT_MAX;
+        int max_profit=0;
+        for(int i=0;i<n;i++){
+            if(prices[i] < minPrice){
+                minPrice=prices[i];
+            }else{
+            max_profit=max(max_profit , prices[i]-minPrice);
+            }
+
+        }
+        return max_profit;
+    }
+};
